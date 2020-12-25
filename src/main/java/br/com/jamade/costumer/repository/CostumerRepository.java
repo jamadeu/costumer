989868dd7226ef77810c1573a7898a2517128bd6
Jean Amadeu <jean.amadeu2@gmail.com>
2020-12-25 07:31:59 -0300

    feat: create CostumerRepository

--- src/main/java/br/com/jamade/costumer/repository/CostumerRepository.java
+++ src/main/java/br/com/jamade/costumer/repository/CostumerRepository.java
@@ -2,6 +2,8 @@ package br.com.jamade.costumer.repository;
 
 import br.com.jamade.costumer.domain.Costumer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CostumerRepository extends JpaRepository<Costumer, Long> {
 }
