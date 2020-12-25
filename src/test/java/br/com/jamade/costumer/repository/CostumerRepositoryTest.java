2d23ee4dcd7886e3baa7588ac74550257993449f
Jean Amadeu <jean.amadeu2@gmail.com>
2020-12-25 08:00:07 -0300

    test: CostumerRepositoryTest findByEmail tests

--- src/test/java/br/com/jamade/costumer/repository/CostumerRepositoryTest.java
+++ src/test/java/br/com/jamade/costumer/repository/CostumerRepositoryTest.java
@@ -1,9 +1,14 @@
 package br.com.jamade.costumer.repository;
 
+import br.com.jamade.costumer.domain.Costumer;
+import br.com.jamade.costumer.util.CostumerCreator;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
 
 @DataJpaTest
 @DisplayName("CostumerRepository tests")
@@ -12,8 +17,24 @@ class CostumerRepositoryTest {
     private CostumerRepository costumerRepository;
 
     @Test
-    @DisplayName("findByEmail returns a costumer when successful")
-    void findByEmail_ReturnsACostumer_WhenSuccessful(){
+    @DisplayName("findByEmail returns an optional of costumer when successful")
+    void findByEmail_ReturnsAnOptionalOfCostumer_WhenSuccessful() {
+        Costumer costumer = costumerRepository.save(CostumerCreator.createValidCostumer());
+        Optional<Costumer> optionalCostumer = costumerRepository.findByEmail(costumer.getEmail());
 
+        Assertions.assertThat(optionalCostumer)
+                .isNotNull()
+                .isNotEmpty()
+                .contains(costumer);
+    }
+
+    @Test
+    @DisplayName("findByEmail returns an empty optional when costumer is not found")
+    void findByEmail_ReturnsAnEmptyOptional_WhenCostumerIsNotFound() {
+        Optional<Costumer> optionalCostumer = costumerRepository.findByEmail("costumer not found");
+
+        Assertions.assertThat(optionalCostumer)
+                .isNotNull()
+                .isEmpty();
     }
 }
