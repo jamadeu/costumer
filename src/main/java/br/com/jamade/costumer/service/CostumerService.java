4d4962b9fbfc187279631ec04219142913cc3e3d
Jean Amadeu <jean.amadeu2@gmail.com>
2020-12-25 09:48:37 -0300

    refact: refactor CostumerService

--- src/main/java/br/com/jamade/costumer/service/CostumerService.java
+++ src/main/java/br/com/jamade/costumer/service/CostumerService.java
@@ -7,26 +7,26 @@ import br.com.jamade.costumer.requests.NewCostumerRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 @RequiredArgsConstructor
 public class CostumerService {
     private final CostumerRepository costumerRepository;
 
-    public Optional<Costumer> findByEmail(String email) {
-        return costumerRepository.findByEmail(email);
+    public Costumer findByEmail(String email) {
+        return costumerRepository.findByEmail(email)
+                .orElseThrow(() -> new BadRequestException("Email already in use"));
     }
 
-    public Optional<Costumer> findByCpf(String cpf) {
-        return costumerRepository.findByCpf(cpf);
+    public Costumer findByCpf(String cpf) {
+        return costumerRepository.findByCpf(cpf)
+                .orElseThrow(() -> new BadRequestException("Cpf already in use"));
     }
 
     public Costumer create(NewCostumerRequest newCostumerRequest) {
-        if (findByCpf(newCostumerRequest.getCpf()).isPresent()) {
+        if (costumerRepository.findByCpf(newCostumerRequest.getCpf()).isPresent()) {
             throw new BadRequestException("Cpf already in use");
         }
-        if (findByEmail(newCostumerRequest.getEmail()).isPresent()) {
+        if (costumerRepository.findByEmail(newCostumerRequest.getEmail()).isPresent()) {
             throw new BadRequestException("Email already in use");
         }
         return costumerRepository.save(newCostumerRequest.toCostumer());
