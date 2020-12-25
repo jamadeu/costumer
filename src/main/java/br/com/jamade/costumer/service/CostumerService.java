accc14bbf4def285b02fed97ed506394d36e53b6
Jean Amadeu <jean.amadeu2@gmail.com>
2020-12-25 09:09:34 -0300

    feat: CostumerService add method create

--- src/main/java/br/com/jamade/costumer/service/CostumerService.java
+++ src/main/java/br/com/jamade/costumer/service/CostumerService.java
@@ -1,7 +1,9 @@
 package br.com.jamade.costumer.service;
 
 import br.com.jamade.costumer.domain.Costumer;
+import br.com.jamade.costumer.exception.BadRequestException;
 import br.com.jamade.costumer.repository.CostumerRepository;
+import br.com.jamade.costumer.requests.NewCostumerRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,5 +20,15 @@ public class CostumerService {
 
     public Optional<Costumer> findByCpf(String cpf) {
         return costumerRepository.findByCpf(cpf);
+    }
+
+    public Costumer create(NewCostumerRequest newCostumerRequest) {
+        if (findByCpf(newCostumerRequest.getCpf()).isPresent()) {
+            throw new BadRequestException("Cpf already in use");
+        }
+        if (findByEmail(newCostumerRequest.getEmail()).isPresent()) {
+            throw new BadRequestException("Email already in use");
+        }
+        return costumerRepository.save(newCostumerRequest.toCostumer());
     }
 }
