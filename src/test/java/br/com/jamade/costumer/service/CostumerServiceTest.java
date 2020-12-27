11aa067a269e708f6002b89da6294697ab61324a
Jean Amadeu <jean.amadeu2@gmail.com>
2020-12-27 08:39:06 -0300

    refactor: CostumerServiceTest

--- src/test/java/br/com/jamade/costumer/service/CostumerServiceTest.java
+++ src/test/java/br/com/jamade/costumer/service/CostumerServiceTest.java
@@ -15,6 +15,8 @@ import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
@@ -30,53 +32,56 @@ class CostumerServiceTest {
     @BeforeEach
     void setup() {
         BDDMockito.when(costumerRepositoryMock.findByEmail(ArgumentMatchers.anyString()))
-                .thenReturn(Optional.of(CostumerCreator.createValidCostumer()));
+                .thenReturn(Optional.empty());
         BDDMockito.when(costumerRepositoryMock.findByCpf(ArgumentMatchers.anyString()))
-                .thenReturn(Optional.of(CostumerCreator.createValidCostumer()));
+                .thenReturn(Optional.empty());
         BDDMockito.when(costumerRepositoryMock.save(ArgumentMatchers.any(Costumer.class)))
                 .thenReturn(CostumerCreator.createValidCostumer());
     }
 
     @Test
-    @DisplayName("findByEmail returns a costumer when successful")
-    void findByEmail_ReturnsCostumer_WhenSuccessful() {
-        Costumer costumer = CostumerCreator.createValidCostumer();
-        Costumer foundedCostumer = costumerService.findByEmail(costumer.getEmail());
+    @DisplayName("checkEmail returns status code OK when email is not in use")
+    void findByEmail_ReturnsStatusCodeOk_WhenEmailIsNotInUse() {
+        ResponseEntity<String> response = costumerService.checkEmail("email@test.com");
 
-        Assertions.assertThat(foundedCostumer)
-                .isNotNull()
-                .isEqualTo(costumer);
+        Assertions.assertThat(response).isNotNull();
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Assertions.assertThat(response.getBody()).isEqualTo("email@test.com not in use");
+
     }
 
     @Test
-    @DisplayName("findByEmail throws BadRequestException when costumer is not found")
-    void findByEmail_ThrowsBadRequestException_WhenCostumerIsNotFound() {
+    @DisplayName("findByEmail throws BadRequestException when email already in use")
+    void findByEmail_ThrowsBadRequestException_WhenEmailIsAlreadyInUse() {
+        Costumer costumer = CostumerCreator.createValidCostumer();
         BDDMockito.when(costumerRepositoryMock.findByEmail(ArgumentMatchers.anyString()))
-                .thenReturn(Optional.empty());
+                .thenReturn(Optional.of(costumer));
+        String email = costumer.getEmail();
 
         Assertions.assertThatExceptionOfType(BadRequestException.class)
-                .isThrownBy(() -> costumerService.findByEmail("costumer not found"));
+                .isThrownBy(() -> costumerService.checkEmail(email));
     }
 
     @Test
-    @DisplayName("findByCpf returns a costumer when successful")
-    void findByCpf_ReturnsCostumer_WhenSuccessful() {
+    @DisplayName("checkCpf returns status code OK when cpf is not in use")
+    void checkCpf_ReturnsStatusCodeOk_WhenCpfIsNotInUse() {
+        ResponseEntity<String> response = costumerService.checkCpf("265.075.120-79");
+
+        Assertions.assertThat(response).isNotNull();
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Assertions.assertThat(response.getBody()).isEqualTo("265.075.120-79 not in use");
+    }
+
+    @Test
+    @DisplayName("checkCpf throws BadRequestException when cpf already in use")
+    void checkCpf_ThrowsBadRequestException_WhenCpfIsAlreadyInUse() {
         Costumer costumer = CostumerCreator.createValidCostumer();
-        Costumer foundedCostumer = costumerService.findByCpf(costumer.getCpf());
-
-        Assertions.assertThat(foundedCostumer)
-                .isNotNull()
-                .isEqualTo(costumer);
-    }
-
-    @Test
-    @DisplayName("findByCpf thorws BadRequestException when costumer is not found")
-    void findByCpf_ReturnsAnEmptyOptional_WhenCostumerIsNotFound() {
         BDDMockito.when(costumerRepositoryMock.findByCpf(ArgumentMatchers.anyString()))
-                .thenReturn(Optional.empty());
+                .thenReturn(Optional.of(costumer));
+        String cpf = costumer.getCpf();
 
         Assertions.assertThatExceptionOfType(BadRequestException.class)
-                .isThrownBy(() -> costumerService.findByCpf("costumer not found"));
+                .isThrownBy(() -> costumerService.checkEmail(cpf));
     }
 
     @Test
